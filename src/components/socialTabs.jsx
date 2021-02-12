686a00b2caf4d1c3c352880a293ee06943b1c2ee
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2021-02-12 17:45:27 +0530

    completed social-tabs

--- src/components/socialTabs.jsx
+++ src/components/socialTabs.jsx
@@ -1,15 +1,25 @@
+/**@jsxImportSource @emotion/core jsx */
+import { css } from "@emotion/core";
+
+function AnchorImage({ src, alt, href }) {
+    return (
+        <a
+            href={href ? href : "#"}
+            css={css`
+                margin: 0 1.5rem;
+            `}
+        >
+            <img src={src} alt={alt} />
+        </a>
+    );
+}
+
 export function SocialTabs() {
     return (
         <section>
-            <a href="#">
-                <img src="icon-facebook.svg" alt="facebook" />
-            </a>
-            <a href="#">
-                <img src="icon-pinterest.svg" alt="facebook" />
-            </a>
-            <a href="#">
-                <img src="icon-instagram.svg" alt="facebook" />
-            </a>
+            <AnchorImage src="icon-facebook.svg" alt="facebook" />
+            <AnchorImage src="icon-pinterest.svg" alt="pinterest" />
+            <AnchorImage src="icon-instagram.svg" alt="instagram" />
         </section>
     );
 }
